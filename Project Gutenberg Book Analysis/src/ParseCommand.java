26434fbf53495b4eee605edac728ceaaea471416
ethanarroyo68 <ethanarroyo68@gmail.com>
2025-09-22 14:08:46 -0600

    finally set up i think lol

---
+++ Project Gutenberg Book Analysis/src/ParseCommand.java
@@ -0,0 +1,9 @@
+public class ParseCommand {
+    public static void run(String folderPath) {
+        // TODO: walk folder, read .txt files
+        // TODO: clean Gutenberg headers/footers
+        // TODO: paragraph tokenize (\n\n)
+        // TODO: store in DB
+    }
+}
+
---
+++ Project Gutenberg Book Analysis/src/Storage.java
@@ -0,0 +1,16 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Storage {
+    private static final String URL = "jdbc:sqlite:gutenberg.db";
+
+    public static Connection connect() throws SQLException {
+        return DriverManager.getConnection(URL);
+    }
+
+    public static void init() {
+        // TODO: create schema if not exists
+        // book(id, title?), paragraph(id, book_id, text)
+    }
+}
